7d9567738d1af5b907543598f6608fc33c9e0627
km127pl <km127pl@gmail.com>
2023-06-16 23:03:39 +0200

    Initial commit

---
+++ src/main/java/us/km127pl/chatcore/ChatCore.java
@@ -0,0 +1,17 @@
+package us.km127pl.chatcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
